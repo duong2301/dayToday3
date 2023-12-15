d3d93ea7c63902a7123ec608c9a45b9d7ff8c018
duong2301 <151597548+duong2301@users.noreply.github.com>
2023-12-15 18:29:02 +0700

    Update Main.java

--- src/main/java/xmlconfig/Main.java
+++ src/main/java/xmlconfig/Main.java
@@ -1,20 +1,20 @@
 package xmlconfig;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import pojobeans.AccountService;
+import pojobeans.AccountServices;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("xmlconfiguration/beans.xml");
-        AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+         AccountServices accountServices = applicationContext.getBean("accountService", AccountServices.class);
 
         System.out.println("Before money transfer");
-        System.out.println("Account 1 balance:"+accountService.getAccount(1).getBalance());
-        System.out.println("Account 2 balance:"+accountService.getAccount(2).getBalance());
+        System.out.println("Account 1 balance:"+accountServices.getAccount(1).getBalance());
+        System.out.println("Account 2 balance:"+accountServices.getAccount(2).getBalance());
 
-        accountService.transferMoney(1,2,5.0);
+        accountServices.transferMoney(1,2,5.0);
         System.out.println("==================");
         System.out.println("After money transfer");
-        System.out.println("Acount 1 balance:"+accountService.getAccount(1).getBalance());
-        System.out.println("Acount 2 balance:"+accountService.getAccount(2).getBalance());
+        System.out.println("Acount 1 balance:"+accountServices.getAccount(1).getBalance());
+        System.out.println("Acount 2 balance:"+accountServices.getAccount(2).getBalance());
 
 
     }
